b6b69a773f86c46d90d66ce5a03a493fcb3b73d5
Felipe Campos <Felipe.Campos@sou.inteli.edu.br>
2024-02-07 11:53:36 -0300

    feat(ponderada1): troquei o sensor escolhido

--- ponderada1/publisher.py
+++ ponderada1/publisher.py
@@ -2,27 +2,29 @@ import paho.mqtt.client as mqtt
 import time
 import random
 
-# Configuração do cliente
-client = mqtt.Client("Sensor de radiação(Publisher)")
+topicArray = ["RED","OX","NH3"]
 
-# Conecte ao broker
+# Configuração do cliente
+client = mqtt.Client("Sensor de poluição no ar(Publisher)")
+
+# Conecte ao broker 
 client.connect("localhost", 1892, 60)
 
-def verify_status(status_code):
+def verify_status(status_code, topic):
     if status_code == 0:
-        print(f"Publicado: {message}")
+        print(f"Publicado: {message}, no tópico: {topic}")
     else:
         print("Falhou em publicar mensagem")
-
-
 
 # Loop para publicar mensagens continuamente
 try:
     while True:
-        message = f"{random.randint(1,1280)}"   
-        result = client.publish("test/radiation", message)
-        verify_status(result[0])
-        time.sleep(2)
+        sensorValueArray = [random.uniform(1,1000), random.uniform(0.05,10), random.uniform(1,300)]
+        for topic, sensorValue in zip(topicArray,sensorValueArray):
+            message = sensorValue  
+            result = client.publish(f"MiCS/{topic}", message)
+            verify_status(result[0], topic)
+        time.sleep(5)
 except KeyboardInterrupt:
     print("Publicação encerrada")
 
--- ponderada1/subscriber.py
+++ ponderada1/subscriber.py
@@ -1,27 +1,31 @@
 import paho.mqtt.client as mqtt
 
+topicArray = ["RED","OX","NH3"]
+
 # Callback quando uma mensagem é recebida do servidor.
 def on_message(client, userdata, message):
     print(f"\nRecebido: {message.payload.decode()} no tópico {message.topic}")
-    check_radiation_level(message.payload.decode())
+    # print(message)
+    # check_radiation_level(message.payload.decode())
 
-def check_radiation_level(radiation):
-    if int(radiation) > 1200:
-        print("Radiação acima do normal, tome cuidado.")
-    else:
-        print("Radiação em nível normal.")
+# def check_radiation_level(radiation):
+#     if float(radiation) > 1200:
+#         print("Radiação acima do normal, tome cuidado.")
+#     else:
+#         print("Radiação em nível normal.")
 
 # Callback para quando o cliente recebe uma resposta CONNACK do servidor.
 def on_connect(client, userdata, flags, rc):
     print("Conectado com código de resultado "+str(rc))
     # Inscreva no tópico aqui, ou se perder a conexão e se reconectar, então as
     # subscrições serão renovadas.
-    client.subscribe("test/radiation")
+    client.subscribe(f"MiCS/#")
 
 # Configuração do cliente2
-client = mqtt.Client("Sensor de radiação(Subscriber)")
-client.on_connect = on_connect
-client.on_message = on_message
+client = mqtt.Client("Sensor de poluição no ar(subscriber)")
+for topic in topicArray:
+    client.on_connect = on_connect
+    client.on_message = on_message
 
 # Conecte ao broker
 client.connect("localhost", 1892, 60)
